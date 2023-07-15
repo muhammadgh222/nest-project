537adc4f69955e59bbe00365f8c9e4420dd2634f
muhammadgh222 <dev.muhammad222000@gmail.com>
2023-07-15 17:14:43 +0200

    creating and finding user by id and email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({
+      select: {
+        email: true,
+      },
+    });
+  }
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found!');
+    }
+
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+  remove() {}
+}
+
+const userService = new UsersService({} as any);
