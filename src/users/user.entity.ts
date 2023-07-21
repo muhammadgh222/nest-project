65cf6a4b4cecf0896499f70f46c3e695bc73dce5
muhammadgh222 <dev.muhammad222000@gmail.com>
2023-07-21 15:40:07 +0200

    excluding password from response

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -7,5 +8,6 @@ export class User {
   @Column()
   email: string;
   @Column()
+  @Exclude()
   password: string;
 }
